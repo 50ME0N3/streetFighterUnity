6c87ebb97cf631804fc2c60a6ed4f18b99670146
50ME0N3 <antoine.dvt@eduge.ch>
2021-09-13 09:03:20 +0200

    ajout du player.cs

---
+++ streetFighter/Assets/scripts/player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("TKT FRERO");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
